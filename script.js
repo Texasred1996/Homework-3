5714bfb86bd840a1d046d0216efed9ecb8d91679
Casey Mark <TRmark96@gmail.com>
2022-03-17 15:24:55 -0700

    updating code-coming back to this later when I understand

---
+++ script.js
@@ -0,0 +1,16 @@
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+let charactorArray = [a,b,c,d,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,1,2,3,4,5,6,7,8,9];
+
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+  let passsize = charactorArray.length;
+  passwordText.value = password;
+  
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
